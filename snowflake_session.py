d6ca24fb00f6ee12b9fd43357c9ecfb03577c67a
pranayingle04 <168402473+pranayingle04@users.noreply.github.com>
2025-01-21 22:29:06 +0530

    Create snowflake_session.py

---
+++ snowflake_session.py
@@ -0,0 +1,16 @@
+from snowflake.snowpark import Session
+
+def create_snowflake_session():
+    """
+    Creates and returns a Snowflake session using predefined connection parameters.
+    """
+    connection_parameters = {
+        "account": "WYB12201",
+        "user": "PranayIngle",
+        "password": "Pranayi25#",
+        "role": "Public",
+        "warehouse": "",
+        "database": "SUSTAINABLITY_CORTEX_SEARCH_DOCS",
+        "schema": "Data"
+    }
+    return Session.builder.configs(connection_parameters).create()
